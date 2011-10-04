0a0895c4d4188087d913f0c7c6c6d8597f4ba0ae
Julien L <julien.lord@mail.mcgill.ca>
2011-10-04 17:21:23 -0400

    Draggable window around the canvas and adding jqueryui

---
+++ server/jqueryui/js/draw_pr.js
@@ -0,0 +1,126 @@
+function drawRectangle(p, x, y, width, height) {
+	p.rect(x,y,width,height);
+	p.println("rect: " + x + ", " + y + ", " + width + ", " + height);
+}
+
+function drawText(p, x, y, text) {
+	//text() method goes wonky if args 2,3 aren't typed right
+	x = parseInt(x);
+	y = parseInt(y);
+	p.text(text, x, y);
+}
+
+function handleEvent(cmd, p) {
+	$('#output').text(cmd);
+	
+	items = cmd.split(' ')
+	if (items[0] == "TXT") {
+		drawText(p, items[1], items[2], items[3])
+	} else if (items[0] == "RECT") {
+		drawRectangle(p, items[1], items[2], items[3], items[4])
+	} else if (items[0] == "CLEAR") {
+		clearCanvas(p)
+	}
+}
+
+function sendExpose(ws) {
+	ws.send("EVENT EXPOSE\n")
+}
+
+function sendClick(ws, x, y) {
+	
+	str = "EVENT CLICK " + x + " " + y + "\n"
+	ws.send(str)
+}
+
+function clearCanvas(p) {
+	p.background(255);
+}
+
+function setupProcessing(p) {
+	p.size(500,500);
+	p.background(255);
+	p.noLoop();
+	p.stroke(0);
+	p.strokeWeight(0);
+	p.fill(0);
+}
+
+
+//Standard stuff. Set websocket.
+$(document).ready(function() {
+	
+	var ws = new WebSocket(ws_server);
+	var root_canvas = document.getElementById("root");
+	var context = root_canvas.getContext("2d");
+	//root_canvas.addEventListener("click", OnClick, false);
+
+	//This will need to be more flexible.
+	var canvas = document.getElementById("root");
+	// attaching the sketchProc function to the canvas
+	var p = new Processing(canvas, setupProcessing);
+	clearCanvas(p);
+	
+	ws.onmessage = function(evt) {
+		  handleEvent(evt.data, p)
+	}
+	ws.onopen = function(evt) {
+			$('#conn_status').html('<b>Connected</b>');
+			sendExpose(ws)
+	}
+	ws.onerror = function(evt) {
+			$('#conn_status').html('<b>Error</b>');
+	}
+	ws.onclose = function(evt) {
+			clearCanvas()
+			$('#conn_status').html('<b>Closed</b>');
+	}
+	
+	//This might need to be revisited.
+	function OnClick(e) {
+		alert($(this).offset());
+		var x;
+		var y;
+
+		if (e.pageX != undefined && e.pageY != undefined) {
+				x = e.pageX;
+				y = e.pageY;
+		}
+		else {
+				x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+				y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+		}
+		
+		x -= root_canvas.offsetLeft;
+		y -= root_canvas.offsetTop;
+		alert(x + "," + y);
+		sendClick(ws, x, y)
+	}
+	$(root_canvas).click( function(eventObj) {
+		var x;
+		var y;
+		if (eventObj.pageX != undefined && eventObj.pageY != undefined) {
+			x = eventObj.pageX;
+			y = eventObj.pageY;
+		}
+		else {
+			x = eventObj.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+			y = eventObj.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+		}
+		var str = "x = " + eventObj.clientX + " + " + document.body.scrollLeft + " + " + document.documentElement.scrollLeft + " - " + $(this).offset().left + 
+			"\n" +
+			"y = " + eventObj.clientY + " + " + document.body.scrollTop + " + " + document.documentElement.scrollTop + " - " + $(this).offset().top;
+		x -= $(this).offset().left;
+		y -= $(this).offset().top;
+		Processing.instances[0].println(str);
+		Processing.instances[0].println("(x,y) = (" + x + "," + y + ")" );
+		sendClick(ws, x, y);
+	});
+
+	$(function() {
+		$( ".draggable" ).draggable({cancel: "canvas"});
+		$( "div.draggable, canvas" ).disableSelection();
+	});
+
+
+});
